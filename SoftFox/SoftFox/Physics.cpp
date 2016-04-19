518374f0b216c5cf98e7f05d630c09d96760de17
Sam Wills <samwillsacc@gmail.com>
2016-04-19 20:13:13 +0100

    Implemented attempted isCollision bool

--- SoftFox/SoftFox/Physics.cpp
+++ SoftFox/SoftFox/Physics.cpp
@@ -6,6 +6,19 @@ Physics::Physics()
 {
 }
 
+bool isCollision(SDL_Rect& objectOne, SDL_Rect& objectTwo)
+{
+//check if there is a box collision between two objects
+    if(objectOne.x < objectTwo.x + objectTwo.width && objectOne.x + objectOne.width > objectTwo.x && objectOne.y < objectTwo.y + objectTwo.height && objectOne.y + objectOne.height > objectTwo.y)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
 
 Physics::~Physics()
 {
