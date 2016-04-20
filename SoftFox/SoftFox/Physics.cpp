50e0dd53d71a0943196db330734d260eb16d7185
Sam Wills <sw180283@falmouth.ac.uk>
2016-04-20 10:01:22 +0100

    Implemented attempt at SDL_IntersectRect

--- SoftFox/SoftFox/Physics.cpp
+++ SoftFox/SoftFox/Physics.cpp
@@ -6,20 +6,24 @@ Physics::Physics()
 {
 }
 
-bool isCollision(SDL_Rect& objectOne, SDL_Rect& objectTwo)
-{
-//check if there is a box collision between two objects
-    if(objectOne.x < objectTwo.x + objectTwo.width && objectOne.x + objectOne.width > objectTwo.x && objectOne.y < objectTwo.y + objectTwo.height && objectOne.y + objectOne.height > objectTwo.y)
-    {
-        return true;
-    }
-
-    else
-    {
-        return false;
-    }
-}
-
 Physics::~Physics()
 {
 }
+
+SDL_bool SDL_IntersectRect(const SDL_Rect* A, const SDL_Rect* B, SDL_Rect* results);
+
+/*
+bool Physics::isCollision(SDL_Rect& objectOne, SDL_Rect& objectTwo)
+{
+	//check if there is a box collision between two objects
+	if (objectOne.x < objectTwo.x + objectTwo.width && objectOne.x + objectOne.width > objectTwo.x && objectOne.y < objectTwo.y + objectTwo.height && objectOne.y + objectOne.height > objectTwo.y)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
+*/
--- SoftFox/SoftFox/Physics.h
+++ SoftFox/SoftFox/Physics.h
@@ -1,8 +1,21 @@
 #pragma once
+
+#include "stdafx.h"
+#include "Texture.h"
+
 class Physics
 {
 public:
 	Physics();
 	~Physics();
+
+	//bool isCollision(SDL_Rect& objectOne, SDL_Rect& objectTwo);
+
+private:
+	int x;
+	int y;
+
+	int width;
+	int height;
 };
 
