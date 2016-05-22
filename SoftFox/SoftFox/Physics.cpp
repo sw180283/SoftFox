2627956bca3dbef7db0bdc92046e9c2108781e53
sw180283 <sw180283@falmouth.ac.uk>
2016-05-22 18:14:29 +0100

    Comments added to physics

--- SoftFox/SoftFox/Physics.cpp
+++ SoftFox/SoftFox/Physics.cpp
@@ -10,19 +10,22 @@ Physics::~Physics()
 {
 }
 
+///Checks for collision between two objects and returns boolean
 bool Physics::isCollision(SDL_Rect& objectOne, SDL_Rect& objectTwo)
 {
-	//check if there is a box collision between two objects
+	///Box dimensions for objectOne
 	int objectOneLeft = objectOne.x;
 	int objectOneRight = objectOne.x + objectOne.w;
 	int objectOneTop = objectOne.y;
 	int objectOneBottom = objectOne.y + objectOne.h;
 
+	///Box dimensions for objectTwo
 	int objectTwoLeft = objectTwo.x;
 	int objectTwoRight = objectTwo.x + objectTwo.w;
 	int objectTwoTop = objectTwo.y;
 	int objectTwoBottom = objectTwo.y + objectTwo.h;
 	
+	///Check if objects collide
 	if (objectOneLeft < objectTwoRight && objectOneRight > objectTwoLeft && objectOneTop < objectTwoBottom && objectOneBottom > objectTwoTop)
 	{
 		return true;
